0851afd2236328041314e391a5099224edece20d
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-03-11 13:52:03 +0400

    added mainapiDI in program cs and registered every other DI inside main api DI

--- RealTimeQuizApi/DependencyInjection.cs
+++ RealTimeQuizApi/DependencyInjection.cs
@@ -1,9 +1,17 @@
-﻿namespace RealTimeQuizApi;
+﻿using RealTimeQuiz.Application;
+using RealTimeQuiz.Infrastructure;
+using RealTimeQuiz.Persistence;
+
+namespace RealTimeQuizApi;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddMainApiDI(this IServiceCollection services)
     {
+        services.AddInfrastructureDI()
+            .AddApplicationDI()
+            .AddPersisitenceDi();
+
         return services;
     }
 }
--- RealTimeQuizApi/Program.cs
+++ RealTimeQuizApi/Program.cs
@@ -1,10 +1,14 @@
+using RealTimeQuizApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi("v1");
+
+builder.Services.AddMainApiDI();
 
 var app = builder.Build();
 
