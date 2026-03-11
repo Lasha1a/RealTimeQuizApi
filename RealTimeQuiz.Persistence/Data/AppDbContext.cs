9fa5514331b95a8a66d549483eaab85d65b220f8
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-03-11 14:23:47 +0400

    added appdbcontext class

---
+++ RealTimeQuiz.Persistence/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealTimeQuiz.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeQuiz.Persistence.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public virtual DbSet<Quiz> Quizzes { get; set; }
+    public virtual DbSet<Question> Questions { get; set; }
+    public virtual DbSet<AnswerOption> AnswerOptions { get; set; }
+    public virtual DbSet<QuizAnalytics> QuizAnalytics { get; set; }
+    public virtual DbSet<Response> Responses { get; set; }
+    public virtual DbSet<ResponseAnswer> ResponseAnswers { get; set; }
+    public virtual DbSet<User> Users { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
