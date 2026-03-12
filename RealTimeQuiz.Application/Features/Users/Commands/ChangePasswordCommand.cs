8db93709e092e8a63aa85b7e868f8d972f0d9246
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-03-12 17:59:22 +0400

    added changepasswordcommand

---
+++ RealTimeQuiz.Application/Features/Users/Commands/ChangePasswordCommand.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using RealTimeQuiz.Application.Interfaces.GenericRepo;
+using RealTimeQuiz.Application.Interfaces.PasswordHash;
+using RealTimeQuiz.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeQuiz.Application.Features.Users.Commands;
+
+public record ChangePasswordCommand(
+    Guid UserId,
+    string CurrentPassword,
+    string NewPassword) : IRequest<bool>;
+
+public class ChangePasswordCommandHandler : IRequestHandler<ChangePasswordCommand, bool>
+{
+    private readonly IGenericRepository<User> _userRepository;
+    private readonly IPasswordHasher _passwordHasher;
+
+    public ChangePasswordCommandHandler(
+        IGenericRepository<User> userRepository,
+        IPasswordHasher passwordHasher)
+    {
+        _userRepository = userRepository;
+        _passwordHasher = passwordHasher;
+    }
+
+    public async Task<bool> Handle(ChangePasswordCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetByIdAsync(request.UserId);
+
+        if (user == null)
+            throw new Exception("User not found");
+
+        var isPasswordValid = _passwordHasher.VerifyPassword(request.CurrentPassword, user.PasswordHash);
+
+        if (!isPasswordValid)
+            throw new Exception("Current password is incorrect");
+
+        var hashedPassword = _passwordHasher.HashPassword(request.NewPassword);
+        user.UpdatePassword(hashedPassword);
+
+        _userRepository.Update(user);
+        await _userRepository.SaveChangesAsync();
+
+        return true;
+    }
+}
