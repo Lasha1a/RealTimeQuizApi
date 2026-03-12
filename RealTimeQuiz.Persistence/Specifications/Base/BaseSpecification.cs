fb200a1d7da384b95d7bdd09cf34f71edcd2195f
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-03-12 12:39:33 +0400

    added ispecification and its implementation also added Specification Evaluator

---
+++ RealTimeQuiz.Persistence/Specifications/Base/BaseSpecification.cs
@@ -0,0 +1,57 @@
+﻿using RealTimeQuiz.Application.Interfaces.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeQuiz.Persistence.Specifications.Base;
+
+public class BaseSpecification<T> : ISpecification<T>
+{
+    private Expression<Func<T, bool>>? _criteria;
+    protected BaseSpecification() { }
+
+    protected BaseSpecification(Expression<Func<T, bool>> criteria)
+    {
+        _criteria = criteria;
+    }
+
+    public Expression<Func<T, bool>>? Criteria => _criteria;
+
+    public List<Expression<Func<T, object>>> Includes { get; } = new();
+
+    public Expression<Func<T, object>>? OrderBy { get; private set; }
+
+    public Expression<Func<T, object>>? OrderByDescending { get; private set; }
+
+    public int? Skip { get; private set; }
+
+    public int? Take { get; private set; }
+
+    public bool IsPagingEnabled { get; private set; }
+
+    //protected methods to set the criteria, includes, order by, and paging options
+    protected void AddInclude(Expression<Func<T, object>> includeExpression)
+    {
+        Includes.Add(includeExpression);
+    }
+
+    protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+    {
+        OrderBy = orderByExpression;
+    }
+
+    protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+    {
+        OrderByDescending = orderByDescExpression;
+    }
+
+    protected void ApplyPaging(int skip, int take)
+    {
+        Skip = skip;
+        Take = take;
+        IsPagingEnabled = true;
+    }
+}
---
+++ RealTimeQuiz.Persistence/Specifications/Evualator/SpecificationEvaluator.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealTimeQuiz.Application.Interfaces.Specifications;
+using RealTimeQuiz.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeQuiz.Persistence.Specifications.Evualator;
+
+public class SpecificationEvaluator<T> where T : BaseEntity
+{
+    public static IQueryable<T> GetQuery(IQueryable<T> query, ISpecification<T> spec)
+    {
+        if (spec.Criteria != null)
+            query = query.Where(spec.Criteria);
+
+        query = spec.Includes.Aggregate(query,
+            (current, include) => current.Include(include));
+
+        if (spec.OrderBy != null)
+            query = query.OrderBy(spec.OrderBy);
+
+        if (spec.OrderByDescending != null)
+            query = query.OrderByDescending(spec.OrderByDescending);
+
+        if (spec.IsPagingEnabled)
+            query = query.Skip(spec.Skip!.Value).Take(spec.Take!.Value);
+
+        return query;
+    }
+}
