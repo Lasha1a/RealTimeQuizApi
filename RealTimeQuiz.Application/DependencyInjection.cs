e2f2fa4a65ac85d4385f23f90e21be2cb50e0e33
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-03-12 18:10:38 +0400

    registered mediatr from assembly so it finds every handler by its own

--- RealTimeQuiz.Application/DependencyInjection.cs
+++ RealTimeQuiz.Application/DependencyInjection.cs
@@ -13,6 +13,10 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationDI(this IServiceCollection services)
     {
+        //instead of manually registering request handlers this mediatr finds all handlers and registers by its own
+        services.AddMediatR(cfg =>
+           cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
         return services;
