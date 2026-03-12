66816882a6ff85d0364dd9395a21f5122671f56d
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-03-12 18:06:40 +0400

    added getuserbyId query

---
+++ RealTimeQuiz.Application/Features/Users/Queries/GetUserByIdQuery.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using RealTimeQuiz.Application.DTOs.User;
+using RealTimeQuiz.Application.Interfaces.GenericRepo;
+using RealTimeQuiz.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeQuiz.Application.Features.Users.Queries;
+
+public record GetUserByIdQuery(Guid UserId) : IRequest<UserResponseDto>;
+
+public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, UserResponseDto>
+{
+    private readonly IGenericRepository<User> _userRepository;
+
+    public GetUserByIdQueryHandler(IGenericRepository<User> userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task<UserResponseDto> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetByIdAsync(request.UserId);
+
+        if (user == null)
+            throw new Exception("User not found");
+
+        return new UserResponseDto
+        {
+            Id = user.Id,
+            Username = user.Username,
+            Email = user.Email,
+            CreatedAt = user.CreatedAt
+        };
+    }
+}
