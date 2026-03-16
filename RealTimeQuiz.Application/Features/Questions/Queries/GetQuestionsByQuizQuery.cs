a34f9062c7e8d81b14bc108f32dec80892b214e9
Lashaia <lashatinikashvili29@gmail.com>
2026-03-16 15:47:17 +0400

    added get questions by quiz query and its needed specification

---
+++ RealTimeQuiz.Application/Features/Questions/Queries/GetQuestionsByQuizQuery.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RealTimeQuiz.Application.DTOs.Question;
+using RealTimeQuiz.Application.Interfaces.GenericRepo;
+using RealTimeQuiz.Application.Specifications.Questions;
+using RealTimeQuiz.Domain.Entities;
+
+namespace RealTimeQuiz.Application.Features.Questions.Queries;
+
+public record GetQuestionsByQuizQuery(Guid QuizId) : IRequest<List<QuestionResponseDto>>;
+
+public class GetQuestionsByQuizQueryHandler : IRequestHandler<GetQuestionsByQuizQuery, List<QuestionResponseDto>>
+{
+    private readonly IGenericRepository<Question> _questionRepository;
+
+    public GetQuestionsByQuizQueryHandler(IGenericRepository<Question> questionRepository)
+    {
+        _questionRepository = questionRepository;
+    }
+
+    public async Task<List<QuestionResponseDto>> Handle(GetQuestionsByQuizQuery request, CancellationToken cancellationToken)
+    {
+        // Use spec to include AnswerOptions and order by OrderIndex
+        var spec = new GetQuestionsByQuizSpec(request.QuizId);
+        var questions = await _questionRepository
+            .GetQueryWithSpec(spec)
+            .ToListAsync(cancellationToken);
+
+        return questions.Select(q => new QuestionResponseDto
+        {
+            Id = q.Id,
+            QuizId = q.QuizId,
+            QuestionText = q.QuestionText,
+            QuestionType = q.QuestionType.ToString(),
+            OrderIndex = q.OrderIndex,
+            IsRequired = q.IsRequired,
+            TimeLimitSeconds = q.TimeLimitSeconds,
+            ImageUrl = q.ImageUrl,
+            CreatedAt = q.CreatedAt,
+            AnswerOptions = q.AnswerOptions.Select(a => new AnswerOptionResponseDto
+            {
+                Id = a.Id,
+                OptionText = a.OptionText,
+                OrderIndex = a.OrderIndex,
+                IsCorrect = a.IsCorrect
+            }).ToList()
+        }).ToList();
+    }
+}
---
+++ RealTimeQuiz.Application/Specifications/Questions/GetQuestionsByQuizSpec.cs
@@ -0,0 +1,19 @@
+﻿using RealTimeQuiz.Application.Specifications.Base;
+using RealTimeQuiz.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeQuiz.Application.Specifications.Questions;
+
+public class GetQuestionsByQuizSpec : BaseSpecification<Question>
+{
+    public GetQuestionsByQuizSpec(Guid quizId)
+        : base(q => q.QuizId == quizId)
+    {
+        AddInclude(q => q.AnswerOptions);
+        AddOrderBy(q => q.OrderIndex);
+    }
+}
