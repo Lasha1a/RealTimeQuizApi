43d03da5438def084946dd6a6970462939976fa3
Lashaia <lashatinikashvili29@gmail.com>
2026-03-16 12:59:24 +0400

    added getquizzesbycreator query and its spec

---
+++ RealTimeQuiz.Application/Features/Quizzes/Queries/GetQuizzesByCreatorQuery.cs
@@ -0,0 +1,46 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RealTimeQuiz.Application.DTOs.Quiz;
+using RealTimeQuiz.Application.Interfaces.GenericRepo;
+using RealTimeQuiz.Application.Specifications.Quizzes;
+using RealTimeQuiz.Domain.Entities;
+
+namespace RealTimeQuiz.Application.Features.Quizzes.Queries;
+
+public record GetQuizzesByCreatorQuery(
+    Guid CreatorId,
+    int PageNumber = 1,
+    int PageSize = 10) : IRequest<List<QuizResponseDto>>;
+
+public class GetQuizzesByCreatorQueryHandler : IRequestHandler<GetQuizzesByCreatorQuery, List<QuizResponseDto>>
+{
+    private readonly IGenericRepository<Quiz> _quizRepository;
+
+    public GetQuizzesByCreatorQueryHandler(IGenericRepository<Quiz> quizRepository)
+    {
+        _quizRepository = quizRepository;
+    }
+
+    public async Task<List<QuizResponseDto>> Handle(GetQuizzesByCreatorQuery request, CancellationToken cancellationToken)
+    {
+        var spec = new GetQuizzesByCreatorSpec(request.CreatorId, request.PageNumber, request.PageSize);
+        var quizzes = await _quizRepository.GetQueryWithSpec(spec)
+            .ToListAsync(cancellationToken);
+
+        return quizzes.Select(quiz => new QuizResponseDto
+        {
+            Id = quiz.Id,
+            Title = quiz.Title,
+            Description = quiz.Description,
+            Type = quiz.Type.ToString(),
+            Visibility = quiz.Visibility.ToString(),
+            AccessCode = quiz.AccessCode,
+            IsAnonymousAllowed = quiz.IsAnonymousAllowed,
+            IsActive = quiz.IsActive,
+            StartsAt = quiz.StartsAt,
+            EndsAt = quiz.EndsAt,
+            CreatedAt = quiz.CreatedAt,
+            CreatorUsername = quiz.Creator.Username
+        }).ToList();
+    }
+}
---
+++ RealTimeQuiz.Application/Specifications/Quizzes/GetQuizzesByCreatorSpec.cs
@@ -0,0 +1,21 @@
+﻿using RealTimeQuiz.Application.Specifications.Base;
+using RealTimeQuiz.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeQuiz.Application.Specifications.Quizzes
+{
+    public class GetQuizzesByCreatorSpec : BaseSpecification<Quiz>
+    {
+        public GetQuizzesByCreatorSpec(Guid creatorId, int pageNumber, int pageSize)
+        : base(q => q.CreatorId == creatorId)
+        {
+            AddInclude(q => q.Creator);
+            AddOrderByDescending(q => q.CreatedAt);
+            ApplyPaging((pageNumber - 1) * pageSize, pageSize);
+        }
+    }
+}
