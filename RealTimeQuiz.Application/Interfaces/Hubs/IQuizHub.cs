84d4fac0596442566d513773356f49196c73b528
Lashaia <lashatinikashvili29@gmail.com>
2026-03-18 11:48:53 +0400

    added signalr and hub class

---
+++ RealTimeQuiz.Application/Interfaces/Hubs/IQuizHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeQuiz.Application.Interfaces.Hubs;
+
+public interface IQuizHub
+{
+    Task AnswerSubmitted(Guid quizId, object stats);
+    Task QuizCompleted(Guid quizId);
+    Task ParticipantJoined(Guid quizId, int TotalParticiapnts);
+}
