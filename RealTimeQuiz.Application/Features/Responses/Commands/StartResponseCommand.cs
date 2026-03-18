5361e7ddf8c42e2ef843989143a2cd169c828422
Lashaia <lashatinikashvili29@gmail.com>
2026-03-18 13:12:52 +0400

    added startresponse command and injected my hub context inside it

--- RealTimeQuiz.Application/Features/Responses/Commands/StartResponseCommand.cs
+++ RealTimeQuiz.Application/Features/Responses/Commands/StartResponseCommand.cs
@@ -1,6 +1,7 @@
 ﻿
 using MediatR;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 using RealTimeQuiz.Application.DTOs.Response;
 using RealTimeQuiz.Application.Interfaces.GenericRepo;
 using RealTimeQuiz.Application.Interfaces.Hubs;
@@ -18,6 +19,74 @@ public class StartResponseCommandHandler : IRequestHandler<StartResponseCommand,
 {
     private readonly IGenericRepository<Response> _responseRepository;
     private readonly IGenericRepository<Quiz> _quizRepository;
-    private readonly IHubContext<QuizHub, IQuizHub> _hubContext;
+    private readonly IQuizHubService _quizHubService;
+
+    public StartResponseCommandHandler(
+        IGenericRepository<Response> responseRepository,
+        IGenericRepository<Quiz> quizRepository,
+        IQuizHubService quizHubService)
+    {
+        _responseRepository = responseRepository;
+        _quizRepository = quizRepository;
+        _quizHubService = quizHubService;
+    }
+
+    public async Task<ResponseDto> Handle(StartResponseCommand request, CancellationToken cancellationToken)
+    {
+        var quiz = await _quizRepository.GetByIdAsync(request.QuizId);
+
+        if(quiz == null)
+        {
+            throw new KeyNotFoundException("Quiz not found");
+        }
+
+        // Check if quiz is active
+        if (!quiz.IsActive)
+            throw new InvalidOperationException("Quiz is not active");
+
+        //check if anonnymous is allowed
+        if(!quiz.IsAnonymousAllowed && request.UserId == null)
+        {
+            throw new UnauthorizedAccessException("This quiz requires authentication");
+        }
+
+        //ip duplicate prevention
+        var existingResponse = await _responseRepository
+            .GetAll()
+            .FirstOrDefaultAsync(r =>
+               r.QuizId == request.QuizId &&
+               r.IpAddressHash == request.IpAddressHash,
+               cancellationToken);
+
+        if (existingResponse != null)
+            throw new InvalidOperationException("You have already responded to this quiz");
+
+        var response = Response.Create(
+            request.QuizId,
+            request.SessionId,
+            request.IpAddressHash,
+            request.UserId);
+
+        await _responseRepository.AddAsync(response);
+        await _responseRepository.SaveChangesAsync();
+
+        //get toal particiapnts for this quiz
+        var totalParticiapnts = await _responseRepository
+            .GetAll()
+            .CountAsync(r => r.QuizId == request.QuizId, cancellationToken);
+
+        //notify with signal
+        await _quizHubService.NotifyParticipantJoined(request.QuizId, totalParticiapnts);
+
+        return new ResponseDto
+        {
+            Id = response.Id,
+            QuizId = response.QuizId,
+            UserId = response.UserId,
+            SessionId = response.SessionId,
+            StartedAt = response.StartedAt,
+            CompletedAt = response.CompletedAt
+        };
+    }
 }
 
