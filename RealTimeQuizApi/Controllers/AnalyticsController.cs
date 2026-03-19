dfb6c630f65f97c2fe7b903dc6776587f4af156c
Lashaia <lashatinikashvili29@gmail.com>
2026-03-19 14:39:35 +0400

    added analytis controller

---
+++ RealTimeQuizApi/Controllers/AnalyticsController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RealTimeQuiz.Application.Features.Analytics.Queries;
+
+namespace RealTimeQuizApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AnalyticsController : ControllerBase
+{
+    private readonly ISender _mediator;
+
+    public AnalyticsController(ISender mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("{quizId}")]
+    [Authorize]
+    public async Task<IActionResult> Analytics( Guid quizId)
+    {
+        
+        var query = new GetQuizAnalyticsQuery(quizId);
+        var result = await _mediator.Send(query);
+        return Ok(result);
+
+    }
+}
