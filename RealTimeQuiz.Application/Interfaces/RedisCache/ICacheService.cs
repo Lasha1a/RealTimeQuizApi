9d878b2175f2aa3f78d5a12360151d95ee40ab00
Lashaia <lashatinikashvili29@gmail.com>
2026-03-19 16:16:08 +0400

    added caching service

---
+++ RealTimeQuiz.Application/Interfaces/RedisCache/ICacheService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeQuiz.Application.Interfaces.RedisCache;
+
+public interface ICacheService
+{
+    Task<T> GetAsync<T>(string key);
+    Task SetAsync<T>(string key, T value, TimeSpan? expiry = null);
+    Task RemoveAsync(string key);
+}
---
+++ RealTimeQuiz.Infrastructure/Services/RedisCache/CacheService.cs
@@ -0,0 +1,66 @@
+﻿
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using RealTimeQuiz.Application.Interfaces.RedisCache;
+using System.Text.Json;
+
+namespace RealTimeQuiz.Infrastructure.Services.RedisCache;
+
+public class CacheService : ICacheService
+{
+    private readonly IDistributedCache _cache;
+    private readonly ILogger<CacheService> _logger;
+
+    public CacheService(IDistributedCache cache, ILogger<CacheService> logger)
+    {
+        _cache = cache;
+        _logger = logger;
+    }
+
+    public async Task<T?> GetAsync<T>(string key)
+    {
+        try
+        {
+            var data = await _cache.GetStringAsync(key);
+
+            if (data == null) return default;
+
+            return JsonSerializer.Deserialize<T>(data);
+        }
+        catch(Exception ex) 
+        {
+                _logger.LogError(ex, "Error getting cache key {Key}", key);
+                return default;
+        }
+     }
+
+    public async Task RemoveAsync(string key)
+    {
+        try
+        {
+            await _cache.RemoveAsync(key);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error removing cache key {Key}", key);
+        }
+    }
+
+    public async Task SetAsync<T>(string key, T value, TimeSpan? expiry = null)
+    {
+        try
+        {
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = expiry ?? TimeSpan.FromMinutes(10)
+            };
+            var data = JsonSerializer.Serialize(value);
+
+            await _cache.SetStringAsync(key, data, options);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error setting cache key {Key}", key);
+        }
+    }
+}
