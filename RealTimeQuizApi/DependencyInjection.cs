54dddc21d6724879118c7f3ee37a4a6e07add5e1
Lashaia <lashatinikashvili29@gmail.com>
2026-03-19 15:11:02 +0400

    added rate limiting configuration in main api

--- RealTimeQuizApi/DependencyInjection.cs
+++ RealTimeQuizApi/DependencyInjection.cs
@@ -1,6 +1,8 @@
-﻿using RealTimeQuiz.Application;
+﻿using Microsoft.AspNetCore.RateLimiting;
+using RealTimeQuiz.Application;
 using RealTimeQuiz.Infrastructure;
 using RealTimeQuiz.Persistence;
+using System.Threading.RateLimiting;
 
 namespace RealTimeQuizApi;
 
@@ -8,6 +10,36 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddMainApiDI(this IServiceCollection services, IConfiguration configurations)
     {
+        services.AddRateLimiter(options =>
+        {
+            options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
+                RateLimitPartition.GetFixedWindowLimiter(
+                    partitionKey: context.Connection.RemoteIpAddress?.ToString() ?? "unknown",
+                    factory: _ => new FixedWindowRateLimiterOptions
+                    {
+                        PermitLimit = 100,
+                        Window = TimeSpan.FromMinutes(1)
+                    }));
+
+            options.AddFixedWindowLimiter("submit", o =>
+            {
+                o.PermitLimit = 30;
+                o.Window = TimeSpan.FromMinutes(1);
+            });
+
+            options.AddFixedWindowLimiter("auth", o =>
+            {
+                o.PermitLimit = 5;
+                o.Window = TimeSpan.FromMinutes(1);
+            });
+
+            options.OnRejected = async (context, token) =>
+            {
+                context.HttpContext.Response.StatusCode = 429;
+                await context.HttpContext.Response.WriteAsync("Too many requests! Please try again later.", token);
+            };
+        });
+
         services.AddInfrastructureDI(configurations)
             .AddApplicationDI()
             .AddPersisitenceDi(configurations);
--- RealTimeQuizApi/Program.cs
+++ RealTimeQuizApi/Program.cs
@@ -31,6 +31,8 @@ if (app.Environment.IsDevelopment())
     app.MapScalarApiReference();
 }
 
+app.UseRateLimiter();
+
 app.UseMiddleware<GlobalExceptionHandler>(); //middleware
 
 app.UseHttpsRedirection();
