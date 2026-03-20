7b69f83447c47503df084ae7522abf13cd21beea
Lashaia <lashatinikashvili29@gmail.com>
2026-03-20 12:51:57 +0400

    run migrations again

---
+++ RealTimeQuiz.Persistence/Migrations/20260320084801_ChangedEntity.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealTimeQuiz.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "users",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "NOW()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "started_at",
+                table: "responses",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "NOW()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "completed_at",
+                table: "responses",
+                type: "timestamp without time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "answered_at",
+                table: "response_answers",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "NOW()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "starts_at",
+                table: "quizzes",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ends_at",
+                table: "quizzes",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "quizzes",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "NOW()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "last_calculated_at",
+                table: "quiz_analytics",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "NOW()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "questions",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "NOW()");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "NOW()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "started_at",
+                table: "responses",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "NOW()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "completed_at",
+                table: "responses",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "answered_at",
+                table: "response_answers",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "NOW()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "starts_at",
+                table: "quizzes",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ends_at",
+                table: "quizzes",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "quizzes",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "NOW()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "last_calculated_at",
+                table: "quiz_analytics",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "NOW()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "questions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "NOW()");
+        }
+    }
+}
